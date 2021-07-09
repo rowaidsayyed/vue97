2dd4c203760bf1c56ab600e2b8ae310a0957e193
rowaidsayyed <rowaidsayyed@gmail.com>
2021-07-09 16:24:57 +0300

    add tailwind

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="bg-primary">
+
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {
+     
+    };
+  },
+};
+</script>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="flex justify-between p-4 md:mx-5">
+    <nav class="flex items-center">
+      <router-link to="/">
+      Home
+      </router-link>
+
+      <!-- <router-link to="/login"> -->
+        <!-- <button class="text-primary md:text-2xl border-0 bg-transparent">دخول</button> -->
+      <!-- </router-link> -->
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
